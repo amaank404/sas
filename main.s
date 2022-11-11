1c46ca588dbf7b4992359a5acfd2e162c8869245
xcodz-dot <71920621+xcodz-dot@users.noreply.github.com>
2022-11-11 22:04:06 +0530

    Added instructions to the main example file

--- main.s
+++ main.s
@@ -1,14 +1,33 @@
-.start _main
+# To run this example, compile it and then run the following command
+#   sasemu.exe emulate -p stdout -m 1024 out.bin
+
+.start _setup
+Mychar:
+    .string "This is an example sentence\n"
+Helloworld:
+    .string "This is another string\n"
+
+print_string:
+    set t1 0b10000000
+1:
+    ldb t0 a0
+    eq t2 t0 zero
+    jif t2 %2f
+    inc a0
+    or t0 t0 t1
+    iow zero t0 0
+    jmp %1b
+2:
+    ret
+
+_setup:
+    set mp %MEMORY_START
+    set sp 1024
+    jmp %_main
 
 _main:
-    # Initialize the counter, upper limit
-    set t0 0
-    set t1 10
-    set t2 false
-1:
-    inc t0   # increment counter
-    eq t2 t0 t1  # check for equality t2 <- (t0 == t1)
-    jif t2 %2f   # If t2 is true, jump forward to local label 2
-    jmp %1b      # else jump back to local label 1
-2:
-    err          # Throw an error intentionally thus exiting
+    set a0 %Mychar
+    call %print_string
+
+    set a0 %Helloworld
+    call %print_string
